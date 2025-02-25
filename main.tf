5263d4f9377f3d0686b8e5a572ab4a2d0b3efff6
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-02-26 02:09:52 +0530

    main.tf added

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "myserver" {
+    ami = "ami-00c257e12d6828491"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "MyWebServer"
+    }
+  
+}
