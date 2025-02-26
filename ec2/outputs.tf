050db7f7535aa79722737d6ea299413f3c5b9644
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-02-26 16:41:24 +0530

    nginx change

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,4 +1,9 @@
-output "aws_instance_public_ip" {
-    value = aws_instance.myserver.public_ip
-  
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.nginxserver.public_ip
+}
+
+output "instance_url" {
+  description = "The URL to access the Nginx server"
+  value       = "http://${aws_instance.nginxserver.public_ip}"
 }
