12fba638452f532c55247d64da788b7231c5a071
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-02-26 16:29:11 +0530

    test

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.88.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.88.0"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.region
