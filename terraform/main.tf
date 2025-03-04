f2cce62686449c65b7eceb2f2f446b51914196ea
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-03-04 21:10:54 +0530

    Rollback

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,31 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.88.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
 
-# provider "aws" {
-#   region = var.aws_region
-# }
+provider "aws" {
+  region = var.aws_region
+}
 
-# # EC2 instance For Nginx setup
-# resource "aws_instance" "nginxserver" {
-#   ami                         = "ami-027951e78de46a00e"
-#   instance_type               = "t2.micro"
-#   subnet_id                   = aws_subnet.public-subnet.id
-#   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
-#   associate_public_ip_address = true
+# EC2 instance For Nginx setup
+resource "aws_instance" "nginxserver" {
+  ami                         = "ami-027951e78de46a00e"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+  associate_public_ip_address = true
 
-#   user_data = <<-EOF
-#             #!/bin/bash
-#             sudo yum install nginx -y
-#             sudo systemctl start nginx
-#             EOF
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo yum install nginx -y
+            sudo systemctl start nginx
+            EOF
 
-#   tags = {
-#     Name = "NginxServer"
-#   }
-# }
+  tags = {
+    Name = "NginxServer"
+  }
+}
