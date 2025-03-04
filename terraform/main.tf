d29a689153792d040f0b7516916a081ff5a46faf
dippi786 <38568643+dippi786@users.noreply.github.com>
2025-03-04 21:34:17 +0530

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,26 +6,42 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = var.aws_region
 }
-
 # EC2 instance For Nginx setup
 resource "aws_instance" "nginxserver" {
   ami                         = "ami-027951e78de46a00e"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public-subnet.id
   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
-  availability_zone           = "us-west-2a"
   associate_public_ip_address = true
   user_data = <<-EOF
             #!/bin/bash
             sudo yum install nginx -y
             sudo systemctl start nginx
             EOF
-
-  tags = {
+            tags = {
     Name = "NginxServer"
+  }
+}
+resource "aws_security_group" "nginx-sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  #Inbound rule for HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Outbound rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "nginx-sg"
   }
 }
